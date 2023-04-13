908da967db96d65ee2c1ee3cd7fbe7afebfda314
Jacoboco <jacob@todandlorna.com>
2023-04-12 20:12:39 -0600

    feat: AND gate

---
+++ src/basics/and.tsx
@@ -0,0 +1,68 @@
+import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
+import {Circle,Polygon, NodeProps, Node, Spline, Knot, Txt, Rect} from '@motion-canvas/2d/lib/components';
+import { createRef } from '@motion-canvas/core/lib/utils';
+import { SignalValue, SimpleSignal, createSignal } from '@motion-canvas/core/lib/signals';
+import {initial, signal} from '@motion-canvas/2d/lib/decorators';
+import * as colors from '../globalColors' 
+import * as sizes from '../globalSizes' 
+import { Vector2 } from '@motion-canvas/core/lib/types';
+
+export interface AndGateProps extends NodeProps {
+    // properties
+    inputA?: SignalValue<boolean> // This could just be inputs[]
+    inputB?: SignalValue<boolean>
+  }
+  
+export class AndGate extends Node {
+    // implementation
+    private readonly notCircle = createRef<Circle>();
+    private readonly notPolygon = createRef<Polygon>();
+    @initial(false)
+    @signal()
+    public declare inputA: SimpleSignal<boolean, this>;
+    @initial(false)
+    @signal()
+    public declare inputB: SimpleSignal<boolean, this>;
+    public output: SimpleSignal<boolean, this> = createSignal(() => this.inputA() && this.inputB());
+    public readonly inputAPos: Vector2 = new Vector2(-22,50).transformAsPoint(this.localToWorld());
+    public readonly inputBPos: Vector2 = new Vector2(22,50).transformAsPoint(this.localToWorld());
+    public readonly outputPos: Vector2 = new Vector2(0,-54).transformAsPoint(this.localToWorld());
+
+    public constructor(props?: AndGateProps) {
+        super({
+            ...props,
+        })
+        this.add(
+            <>
+            {/* Test Rect for sizing purposes */}
+            {/* <Rect fill="#ffffff" width={120} height={120}/> */}
+            <Rect
+                ref={this.notPolygon}
+                fill={colors.GATE_COLOR}
+                y={21}
+                width={90}
+                height={70}
+                lineWidth={sizes.WIRE_WIDTH}
+                stroke={()=>this.output() ? colors.POWERED_COLOR:colors.OFF_COLOR}
+                lineJoin="round"
+            />
+            <Circle
+                fill={colors.GATE_COLOR}
+                size={90}
+                y={-10}
+                lineWidth={sizes.WIRE_WIDTH}
+                stroke={()=>this.output() ? colors.POWERED_COLOR:colors.OFF_COLOR}
+                startAngle={180}
+            />
+            <Txt
+                text="AND"
+                fill={colors.TEXT_COLOR}
+                fontFamily="Helvetica"
+                fontSize={20}
+                fontWeight={700}
+                y={7}
+            />
+            </>
+        );
+    }
+}
