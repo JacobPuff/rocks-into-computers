bcb7432fe9b99f0ce2810eab812c19dd484e4cd2
Jacoboco <jacob@todandlorna.com>
2023-04-12 22:09:23 -0600

    feat: example with all gates so far

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -13,11 +13,12 @@ import { Txt } from '@motion-canvas/2d/lib/components';
 export default makeScene2D(function* (view) {
   const firstNot = createRef<NotGate>();
   const orGate = createRef<OrGate>();
+  const andGate = createRef<AndGate>();
   const wires: Wire[] = [];
   const firstInput = createSignal(true)
   const secondInput = createSignal(true)
 
-  const testXOffset = -100;
+  const testXOffset = -150;
   const testYOffset = -50;
 
   view.fill(BACKGROUND_COLOR);
@@ -32,21 +33,32 @@ export default makeScene2D(function* (view) {
       />
       <OrGate
         ref={orGate}
-        x={200 + testXOffset}
+        x={170 + testXOffset}
         y={100 + testYOffset}
         rotation={90}
         isNOR={false}
-        inputA={firstNot().output}
+        inputA={firstInput}
         inputB={secondInput}
       />
+      <AndGate
+        ref={andGate}
+        x={350 + testXOffset}
+        y={50 + testYOffset}
+        rotation={90}
+        isNAND={false}
+        inputA={firstNot().output}
+        inputB={orGate().output}
+      />
       
+      {/* firstInput base wire */}
       <Wire
         ref={makeRef(wires, wires.length)}
         powered={firstInput}
         points={[
-          [-120 + testXOffset, 100 + testYOffset],
+          [-120 + testXOffset, 200 + testYOffset],
           [-120 + testXOffset, -100 + testYOffset]]}
       />
+      {/* firstInput base wire, NOT gate input */}
       <Wire
         ref={makeRef(wires, wires.length)}
         jointStart
@@ -56,36 +68,62 @@ export default makeScene2D(function* (view) {
           firstNot().inputPos
         ]}
       />
+      {/* first Input base wire, OR gate input A */}
+      <Wire
+        ref={makeRef(wires, wires.length)}
+        powered={firstInput}
+        jointStart
+        points={[
+          [-120 + testXOffset,orGate().inputAPos.y],
+          [100 + testXOffset,orGate().inputAPos.y],
+          orGate().inputAPos
+        ]}
+      />
 
+      {/* secondInput base wire, OR gate input B*/}
+      <Wire
+        ref={makeRef(wires, wires.length)}
+        powered={secondInput}
+        points={[
+          [70 + testXOffset, 200 + testYOffset],
+          [70 + testXOffset,orGate().inputBPos.y],
+          [70 + testXOffset,orGate().inputBPos.y],
+          orGate().inputBPos
+        ]}
+      />
+
+      {/* AND gate inputs */}
       <Wire
         ref={makeRef(wires, wires.length)}
         powered={firstNot().output}
         points={[
           firstNot().outputPos,
           [100 + testXOffset, 0 + testYOffset],
-          [100 + testXOffset,orGate().inputAPos.y],
-          orGate().inputAPos
+          [250 + testXOffset, 0 + testYOffset],
+          [250 + testXOffset, andGate().inputAPos.y],
+          andGate().inputAPos
         ]}
       />
       <Wire
         ref={makeRef(wires, wires.length)}
-        powered={secondInput}
-        points={[
-          [100 + testXOffset, 200 + testYOffset],
-          [100 + testXOffset,orGate().inputBPos.y],
-          [100 + testXOffset,orGate().inputBPos.y],
-          orGate().inputBPos
-        ]}
-      />
-
-      <Wire
-        ref={makeRef(wires, wires.length)}
-        jointEnd
         powered={orGate().output}
         points={[
           orGate().outputPos,
-          [orGate().outputPos.x+50,100 + testYOffset]
-        ]}/>
+          [250 + testXOffset,  orGate().outputPos.y],
+          [250 + testXOffset, andGate().inputBPos.y],
+          andGate().inputBPos
+        ]}
+      />
+      {/* AND gate output */}
+      <Wire
+        ref={makeRef(wires, wires.length)}
+        jointEnd
+        powered={andGate().output}
+        points={[
+          andGate().outputPos,
+          [andGate().outputPos.x+50,andGate().outputPos.y]
+        ]}
+      />
     </>
   );
   // Reversed so the typical heirarchy is consistent between wires. Defined first means on bottom.
