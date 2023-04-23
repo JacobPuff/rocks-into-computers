eb3cb7fc22f43a28feeac758096bedf4b06f0dd8
Jacoboco <jacob@todandlorna.com>
2023-04-23 17:25:54 -0600

    fix: add notGate to scenes

--- src/project.ts
+++ src/project.ts
@@ -1,10 +1,11 @@
 import {makeProject} from '@motion-canvas/core';
 
 import example from './scenes/example?scene';
+import notGate from './scenes/notGate?scene';
 import secondScene from './scenes/secondScene?scene';
 // import help from './scenes/help?scene';
 
 export default makeProject({
-  scenes: [example],
+  scenes: [notGate],
   variables: {backgroundColor: '#2c2938'}
 });
