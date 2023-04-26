f7ccb0e21cd6e368f8692fd9f0833730a1b3760f
Jacoboco <jacob@todandlorna.com>
2023-04-25 20:20:21 -0600

    fix: rename AND gate and OR gate scenes

--- src/project.ts
+++ src/project.ts
@@ -2,12 +2,12 @@ import {makeProject} from '@motion-canvas/core';
 
 import example from './scenes/example?scene';
 import notGate from './scenes/notGate?scene';
-import andGate from './scenes/andGate?scene';
-import orGate from './scenes/orGate?scene';
+import andGates from './scenes/andGates?scene';
+import orGates from './scenes/orGates?scene';
 import secondScene from './scenes/secondScene?scene';
 // import help from './scenes/help?scene';
 
 export default makeProject({
-  scenes: [orGate, notGate, andGate],
+  scenes: [notGate, andGates, orGates],
   variables: {backgroundColor: '#2c2938'}
 });
