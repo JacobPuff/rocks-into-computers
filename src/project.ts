4616b3841a56679e6ee5e0fec1133ff2e89ceb2c
Jacoboco <jacob@todandlorna.com>
2023-04-27 22:10:19 -0600

    feat: finish halfAdder slide, remove secondScene

--- src/project.ts
+++ src/project.ts
@@ -5,10 +5,9 @@ import notGate from './scenes/notGate?scene';
 import andGates from './scenes/andGates?scene';
 import orGates from './scenes/orGates?scene';
 import halfAdder from './scenes/halfAdder?scene';
-import secondScene from './scenes/secondScene?scene';
 // import help from './scenes/help?scene';
 
 export default makeProject({
-  scenes: [halfAdder, notGate, andGates, orGates],
+  scenes: [notGate, andGates, orGates, halfAdder],
   variables: {backgroundColor: '#2c2938'}
 });
