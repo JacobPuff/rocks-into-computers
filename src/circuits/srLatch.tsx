4846881b390f627f2765db061363e69e7f452b70
Jacoboco <jacob@todandlorna.com>
2023-05-13 23:26:00 -0600

    style: extra space removed

--- src/circuits/srLatch.tsx
+++ src/circuits/srLatch.tsx
@@ -70,7 +70,7 @@ export class SRLatch extends Node {
                 />
                 <Txt
                     text={"R"}
-                    rotation={()=>this.rotation()*-1 }
+                    rotation={()=>this.rotation()*-1}
                     position={[this.localResetPos.x, this.localResetPos.y - 10]}
                     fill={colors.TEXT_COLOR}
                     fontFamily="Helvetica"
@@ -79,7 +79,7 @@ export class SRLatch extends Node {
                 />
                 <Txt
                     text={"S"}
-                    rotation={()=>this.rotation()*-1 }
+                    rotation={()=>this.rotation()*-1}
                     position={[this.localSetPos.x, this.localSetPos.y - 10]}
                     fill={colors.TEXT_COLOR}
                     fontFamily="Helvetica"
@@ -88,7 +88,7 @@ export class SRLatch extends Node {
                 />
                 <Txt
                     text={"Q"}
-                    rotation={()=>this.rotation()*-1 }
+                    rotation={()=>this.rotation()*-1}
                     position={[this.localOutputPos.x, this.localOutputPos.y + 17]}
                     fill={colors.TEXT_COLOR}
                     fontFamily="Helvetica"
@@ -97,7 +97,7 @@ export class SRLatch extends Node {
                 />
                 <Txt
                     text={"Q̅"}
-                    rotation={()=>this.rotation()*-1 }
+                    rotation={()=>this.rotation()*-1}
                     position={[this.localNotOutputPos.x, this.localNotOutputPos.y + 17]}
                     fill={colors.TEXT_COLOR}
                     fontFamily="Helvetica"
